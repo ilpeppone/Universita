d52092ebaf4985f32523657a51f8bb5eca7a2123
ilpeppone <pepperusso07@gmail.com>
2024-05-17 14:57:55 +0200

    update .DS_Store and .DS_Store

---
+++ Sistemi_operativi/c/SO-2020-06-19-pepp/trova_video.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define DD 200
+#define Dim_Data 7
+#define Dim_Tipo 50
+#define DIM_PATH 250
+static volatile sig_atomic_t counter;
+
+void handler(){
+    printf("Numero di richieste eseguite: %d",counter);
+}
+
+int main(int argc,char **argv){
+    int fd,pid,p1p2[2],p0p2[2];
+    char dir[DD],data[Dim_Data],tipo[Dim_Tipo],file_path[DIM_PATH];
+    if(argc!=2){
+        fprintf(stderr,"Uso: %s dir\n",argv[0]);
+        exit(-1);
+    }
+    
+    if(argv[1][0]=='/'){
+        fprintf("Errore: %s non è il nome di un direttorio realativo\n",argv[1]);
+        exit(-2);
+    }
+
+    sprintf(dir,"%s",argv[1]);
+
+    if((fd=open(dir,O_DIRECTORY))<0){
+        perror("Errore apertura cartella\n");
+        exit(-3);
+    }
+    close(fd);
+    
+    signal(SIGINT,handler);
+
+    while(1){
+        printf("Inserisci il tipo di file da cercare\n");
+        scanf("%s",tipo);
+
+        printf("Inserisci la data di interesse\n");
+        scanf("%s",data);
+
+        sprintf(file_path,"%s/%s.txt",dir,data);
+
+        if((fd=open(file_path,O_RDONLY))<0){
+            perror("Errore apertura file\n");
+            exit(-3);
+        }
+        close(fd);
+        if(pipe(p1p2)<0){
+            perror("Errore pipe p1p2\n");
+            exit(-4);
+        }
+        if((pid=fork())<0){
+            perror("Errore fork p1\n");
+            exit(-5);
+        }
+        if(pid==0){
+            signal(SIGINT,SIG_DFL);
+
+            
+        }
+
+
+
+    }
+
+}
