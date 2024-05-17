f99954a0789cd9e7e9a5c8e90ac01c8fca198b43
ilpeppone <pepperusso07@gmail.com>
2024-05-17 14:05:02 +0200

    update trova_spettacoli.c

--- Sistemi_operativi/c/SO-2021-07-09-pepp/trova_spettacoli.c
+++ Sistemi_operativi/c/SO-2021-07-09-pepp/trova_spettacoli.c
@@ -3,10 +3,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
 #define ROOT_PATH "/home/studente"
 #define DIM_PER 200
 #define DIM_SPECT 200
+#define RIS 2048
 
 static volatile sig_atomic_t count;
 
@@ -15,17 +18,17 @@ void handler(){
 }
 
 int main(int argc, char **argv){
-    int fd,n_spettacoli,p1p2[2],p2p3[2],p0p3[2],pid;
-    char percorso_file [DIM_PER],spettacolo[DIM_SPECT];
+    int fd,n_spettacoli,p1p2[2],p2p3[2],p0p3[2],pid,status,n_read;
+    char percorso_file [DIM_PER],spettacolo[DIM_SPECT],ris[RIS];
 
     if(argc!=2){
         fprintf(stderr,"Uso: %s nomeTeatro\n",argv[0]);
         exit(-1);
     }
 
-    sprintf(percorso_file,"%s/%s",ROOT_PATH,argv[1]);
+    sprintf(percorso_file,"%s/%s.txt",ROOT_PATH,argv[1]);
 
-    if((fd=open("%s",percorso_file,O_RDONLY))<0){
+    if((fd=open(percorso_file,O_RDONLY))<0){
         perror("Errore lettura\n");
         exit(-2);
     }
@@ -37,7 +40,7 @@ int main(int argc, char **argv){
     scanf("%s",spettacolo);
 
     printf("Inserire numeri di spettacoli da trovare:\t");
-    scanf("%d",n_spettacoli);
+    scanf("%d",&n_spettacoli);
 
     while(n_spettacoli!=0){
         if(pipe(p1p2)<0){
@@ -95,13 +98,54 @@ int main(int argc, char **argv){
             perror("Errore p3\n");
             exit(-4);
         }
+        if(pid==0){
+            signal(SIGINT,SIG_DFL);
+
+            char str_n_spettacoli[4];
+
+            close(p1p2[0]);
+            close(p1p2[1]);
+            close(p2p3[1]);
+            close(p0p3[0]);
+            //redirezione stdin
+            close(0);
+            dup(p0p3[0]);
+            close(p0p3[0]);
+            //redirezione stdout
+            close(1);
+            dup(p0p3[1]);
+            close(p0p3[1]);
+            sprintf(str_n_spettacoli,"%d",n_spettacoli);
+            execlp("head","head","-n",str_n_spettacoli,(char *)0);
+            perror("Errore head\n");
+            exit(-5);
+        }
+
+        close(p1p2[0]);
+        close(p1p2[1]);
         
+        close(p2p3[0]);
+        close(p2p3[1]);
 
+        close(p0p3[1]);
 
+        wait(&status);
+        wait(&status);
+        wait(&status);
 
+        n_read=read(p0p3[0],ris,sizeof(ris)-1);
+        if(n_read<0){
+            perror("Error Read");
+            exit(-6);
+        }
 
+        close(p0p3[0]);
+        ris[n_read]='\0';
 
+        printf("%s\n",ris);
+
+        count++;
     }
-
+    printf("Numero di richieste servite: %d",count);
     return 0;
 }
