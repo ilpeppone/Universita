1c216dac0bb5dfcb41d6a83680852b4027d27e3b
ilpeppone <pepperusso07@gmail.com>
2024-05-17 13:37:37 +0200

    update trova_spettacoli.c

--- Sistemi_operativi/c/SO-2021-07-09-pepp/trova_spettacoli.c
+++ Sistemi_operativi/c/SO-2021-07-09-pepp/trova_spettacoli.c
@@ -15,7 +15,7 @@ void handler(){
 }
 
 int main(int argc, char **argv){
-    int fd;
+    int fd,n_spettacoli,p1p2[2],p2p3[2],p0p3[2],pid;
     char percorso_file [DIM_PER],spettacolo[DIM_SPECT];
 
     if(argc!=2){
@@ -31,6 +31,77 @@ int main(int argc, char **argv){
     }
     close(fd);
 
-    
+    signal(SIGINT,handler);
 
+    printf("Inserire nome di uno spettacolo:\t");
+    scanf("%s",spettacolo);
+
+    printf("Inserire numeri di spettacoli da trovare:\t");
+    scanf("%d",n_spettacoli);
+
+    while(n_spettacoli!=0){
+        if(pipe(p1p2)<0){
+            perror("Errore p1p2\n");
+            exit(-3);
+        }
+        if((pid=fork())<0){
+            perror("Errore fork p1\n");
+            exit(-4);
+        }
+        if(pid==0){
+            signal(SIGINT,SIG_DFL);
+
+            close(p1p2[0]);
+
+            close(1);
+            dup(p1p2[1]);
+            close(p1p2[1]);
+
+            execlp("grep","grep",spettacolo,(char *)0);
+            perror("Error Grep\n");
+            exit(-5);
+        }
+        if(pipe(p2p3)<0){
+            perror("Errore p2p3\n");
+            exit(-3);
+        }
+        if((pid=fork())<0){
+            perror("Errore fork p2");
+            exit(-4);
+        }
+        if(pid==0){
+
+            close(p1p2[1]);
+            close(p2p3[0]);
+
+            close(0);
+            dup(p1p2[0]);
+            close(p1p2[0]);
+            
+            
+            close(1);
+            dup(p2p3[1]);
+            close(p2p3[1]);
+
+            execlp("sort","sort","-n",(char *)0);
+            perror("Errore sort\n");
+            exit(-5);
+        }
+        if(pipe(p0p3)<0){
+            perror("Errore p0p3\n");
+            exit(-3);
+        }
+        if((pid=fork())<0){
+            perror("Errore p3\n");
+            exit(-4);
+        }
+        
+
+
+
+
+
+    }
+
+    return 0;
 }
