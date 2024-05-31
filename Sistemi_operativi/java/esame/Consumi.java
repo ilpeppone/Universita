02757749b560d9498893e70ce734a003df5fdfdf
ilpeppone <pepperusso07@gmail.com>
2024-05-31 09:11:11 +0200

    update Main.java

---
+++ Sistemi_operativi/java/esame/Consumi.java
@@ -0,0 +1,14 @@
+package esame;
+
+public class Consumi {
+    protected float consumo = 0.0F;
+
+    public float getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(float consumo) {
+        this.consumo = consumo;
+    }
+    
+}
